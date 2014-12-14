7b650b8ca914a9a8b8eaa3b118669aea88e37783
Linh Bui <linhbui@colgate.edu>
2014-12-14 01:41:56 -0800

    change asteroids to images instead of circles

--- Asteroids/lib/asteroid.js
+++ Asteroids/lib/asteroid.js
@@ -7,25 +7,28 @@
         Asteroids.MovingObject.call(this, params);
     }
 
+    Asteroid.IMAGESRC = "./images/asteroid.png";
+
     Asteroids.Util.inherits(Asteroid, Asteroids.MovingObject);
 
-    var HEX_DIGITS = "0123456789ABCDEF";
-    Asteroid.randomColor = function () {
-        var color = "#";
-        for (var i = 0; i < 6; i++) {
-            color += HEX_DIGITS[Math.floor((Math.random() * 16))];
-        }
+    //var HEX_DIGITS = "0123456789ABCDEF";
+    //Asteroid.randomColor = function () {
+        //var color = "#";
+        //for (var i = 0; i < 6; i++) {
+            //color += HEX_DIGITS[Math.floor((Math.random() * 16))];
+        //}
 
-        return color;
-    }
+        //return color;
+    //}
 
     Asteroid.randomAsteroid = function (maxX, maxY, game) {
         return new Asteroid({
             pos: [maxX * Math.random(), maxY * Math.random()],
                vel: Asteroids.Util.randomVec(Math.random() * 5),
-               radius: Math.random() * (35 - 10) + 10,
-               color: Asteroid.randomColor(),
-               game: game
+               radius: Math.random() * (60 - 20) + 20,
+               color: "#FFFFFF",
+               game: game,
+               imgSrc: Asteroid.IMAGESRC
         })
     }
 })();
--- Asteroids/lib/movingObject.js
+++ Asteroids/lib/movingObject.js
@@ -30,16 +30,23 @@
 
             ctx.fill();
         } else {
-            var image = this.image;
-            var pos = this.pos;
-            var radius = this.radius;
-            var radians = Math.atan2(this.vel[1], this.vel[0]);
+            if (this instanceof Asteroids.Ship) {
+                var image = this.image;
+                var pos = this.pos;
+                var radius = this.radius;
+                var radians = Math.atan2(this.vel[1], this.vel[0]);
 
-            ctx.translate(pos[0], pos[1]);
-            ctx.rotate(radians);
-            ctx.drawImage(image, -radius - 15, -radius - 15, 60, 60);
-            ctx.rotate(-radians);
-            ctx.translate(-pos[0], -pos[1]);
+                ctx.translate(pos[0], pos[1]);
+                ctx.rotate(radians);
+                ctx.drawImage(image, -radius - 15, -radius - 15, 60, 60);
+                ctx.rotate(-radians);
+                ctx.translate(-pos[0], -pos[1]);
+            } else {
+                var image = this.image;
+                var pos = this.pos;
+                var radius = this.radius;
+                ctx.drawImage(image, pos[0] - radius, pos[1] - radius, radius * 2.5, radius * 2.5);
+            }
         }
     }
     
